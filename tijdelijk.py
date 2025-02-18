befd4f579fafab7f14264df17ed9177ab0eff02a
Mart Friederichs <info@mftechniek.com>
2025-02-18 17:53:50 +0100

    funties toegevoegd

--- tijdelijk.py
+++ tijdelijk.py
@@ -1,27 +1,25 @@
-prijzen = {
+from helper import decodeer
+
+def print_aanbieding():
+    prijzen = {
     "aardbei" : 3,
     "vanille" : 4,
     "chocolade" : 5
-}
+    }
 
-aanbieding = prijzen["aardbei"] * 0.8
+    aanbieding = prijzen["aardbei"] * 0.8
 
-reclame_tekst = "Vandaag in de aanbieding: vanille-ijs, 1 liter - slechts €" + str(aanbieding)
-reclame_tekst_2 = reclame_tekst [:62]
-reclame_tekst_3 = reclame_tekst_2.upper() #Hoofdletters
-reclame_tekst_4 = [reclame_tekst_3[:7], #VANDAAG List
-                   reclame_tekst_3[8:10], #IN
-                   reclame_tekst_3[11:13], #DE
-                   reclame_tekst_3[14:24], #AANBIEDING
-                   reclame_tekst_3[26:37], #VANILLE-IJS
-                   reclame_tekst_3[39:46], #1 LITER
-                   reclame_tekst_3[49:56], #SLECHTS
-                   reclame_tekst_3[57:]] #€
+    reclame_tekst = "Vandaag in de aanbieding: vanille-ijs, 1 liter - slechts €" + str(aanbieding)
+    reclame_tekst_2 = reclame_tekst [:62]
+    reclame_tekst_3 = reclame_tekst_2.upper() #Hoofdletters
+    reclame_tekst_4 = reclame_tekst_3.split() 
 
-for item in reclame_tekst_4:
-    if len(item) > 4:
-        print(item.upper())
-    else:
-        print(item.lower())
+    for item in reclame_tekst_4:
+        if len(item) > 4:
+            print(item.upper())
+        else:
+            print(item.lower())
 
 
+decodeer("Aanbieding")
+print_aanbieding()
